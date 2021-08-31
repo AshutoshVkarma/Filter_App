4a5924b8738b6da9211fc6bff6e73690b211f11f
Ashutosh Vishwakarma <vashutosh66@gmail.com>
2021-09-01 02:35:16 +0530

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using Filter_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filter_App.Controllers
+{
+    public class UsersController : Controller
+    {
+        private ApplicationContext context;
+        public UsersController()
+        {
+            context = new ApplicationContext();
+        }
+            // GET: Users
+            public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn(SignInModel model)
+        {
+            var user = context.Users.SingleOrDefault(e => e.Email == model.Email && e.Password == model.Password);
+            if(user!= null)
+            {
+                return RedirectToAction("Index","Product");
+            }
+            else
+            {
+                ViewBag.Msg = "Invalid User";
+            }
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(Users users)
+        {
+            context.Users.Add(users);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Filter_App.Models
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext() : base() { }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Products> Products { get; set; }
+    }
+}
